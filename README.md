416006d75e33b9fb2f72700f73fc6cc105e88e1b
unknown <mananpoddarcse@gmail.com>
2018-08-01 22:18:07 +0530

    recruitments

---
+++ README.md
@@ -0,0 +1,2 @@
+# man-an.github.io
+hey this is the code of my personal website .you can find all the details there
---
+++ topnav.js
@@ -0,0 +1,43 @@
+
+
+        function callabout(){
+        document.getElementById("demo").innerHTML=" <ul><li>dancing</li><li>singing</li><li>interacting with simple people</li><li>travelling</li><li>eating food</li>"
+                                                                }
+
+                function callprojects(){
+                  document.getElementById("demo").innerHTML=
+                  " <ul><li>Maharishi Vidya Mandir Shahdol(nursery to class 3rd)</li><li>Sacred Heart Katni(class 4th to class 8th  )</li><li>Maharishi Vidya Mandir Shahdol(class 9th to class 12th)</li><li>Carrer Point Gurukul(drop year for JEE-2017)</li><li>BTECH in computer science from National Institute Of Technology Surathkal(running) </li>"
+                                           }
+
+                  function callinterests(){
+                          document.getElementById("demo").innerHTML=" <ul><li>AIR 2743 in jee mains 2017</li><li>AIR 10,666 in jee advanced 2017</li><li>93.4% in class 12th CBSE(2016) </li><li>10 cgpa in class 10th</li>"+
+                          "<li>represented school in national cultural meet happpened at bangalore(2015)</li><li> 1st rank in cultural meet of MVM in debate competition(state) </li> </ul>"
+                        }
+                  
+                  function callcontact(){
+                          document.getElementById("demo").innerHTML="<ul><li>&#35; 20,Block 7,NITK,  SURATHKAL</li><li>INDIA</li><li>+91&nbsp;9113536695</li><li><a href=mailto:mananpoddarm@gmail.com style=text-decoration:none>&#9993;"+
+                         " mananpoddarm@gmail.com</a></li></ul>"
+                  }
+                      
+                  function callaboutme(){
+                        document.getElementById("demo").innerHTML=" <div style=font-family: cursive >About Me <br><br>"+
+                       " I believe that when your work is related to your interests results can be outstanding and everyone in happy , for me "+
+                     " my work is more important then the assests re+lated to it."+
+                       " <div style=text-indent: 1em>computer science is a vast field and yet i chose web development because i think this "+
+                       "field has the power to provide solutions to the "+
+                        "much awaited problems of the world.Actually apart from the above stated fact i really enjoy my work developing websites both static and dynamic."+ 
+                        "Also i think i will prefer a job where my love for travel isn't compromised.</div></div>"
+                        
+                        
+                        
+                      
+                  }
+                    
+                                                                                                          
+                                                                                                          
+        
+        
+                                                                 
+                                                   
+       
+        
